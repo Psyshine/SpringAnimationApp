38f68bb50fe92ba6fc3ef939731f4cc3eeee7833
Andry Prokopenko <isp.fline@gmail.com>
2020-02-26 11:00:49 +0100

    Initial Commit

---
+++ SpringAnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpringAnimationApp
+//
+//  Created by Andry Pro on 26.02.2020.
+//  Copyright © 2020 Andry Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
